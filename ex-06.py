9a94fcb6d5cbdd5966232a99cbc1a80db1e41a6e
thiagogcunha86 <thiagogcunha@gmail.com>
2025-04-15 20:58:52 -0300

    Create ex-06.py

---
+++ ex-06.py
@@ -0,0 +1,7 @@
+#6. Menu - Elabore um programa que mostre o seguinte menu na tela:
+print("Cadastro de Clientes")
+print("0 - Fim")
+print("1 - Inclui")
+print("2 - Altera")
+print("3 - Exclui")
+print("4 - Consulta")
