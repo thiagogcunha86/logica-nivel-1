2e8e80d4217b0f93caf083de75c78b7064728352
thiagogcunha86 <thiagogcunha@gmail.com>
2025-04-15 20:50:03 -0300

    Create ex-02.py

---
+++ ex-02.py
@@ -0,0 +1,4 @@
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+print("THIAGO GOMES")
+print("RIO DE JANEIRO")
+print("DUQUE DE CAXIAS")
