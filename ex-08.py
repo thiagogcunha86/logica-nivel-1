50898ec3bbf3180ca7956195fa9383ee70547260
thiagogcunha86 <thiagogcunha@gmail.com>
2025-04-15 21:29:56 -0300

    Create ex-08.py

---
+++ ex-08.py
@@ -0,0 +1,4 @@
+#8. Implemente um programa que na primeira linha faça um print perguntando o nome do usuário e, na linha 2, chame a função input() para capturar o valor do teclado e armazenar na memória do computador. No fim, de boas vindas ao usuário com o seu nome aparecendo na tela.
+print("Qual é o seu nome?")
+nome=input ()
+print("Seja bem vindo", nome +"!")
