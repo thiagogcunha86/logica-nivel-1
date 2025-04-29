5db576cb22099a07a486e95ecce52a2fd1c9102e
thiagogcunha86 <thiagogcunha@gmail.com>
2025-04-29 20:38:20 -0300

    Create ex-10.py

---
+++ ex-10.py
@@ -0,0 +1,12 @@
+#9 - escreva um software que some dois números.
+# Soma de dois números
+
+# Solicita os números ao usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+#soma
+soma = num1 + num2
+
+#Imprime resultado
+print("A soma dos dois números é:", soma)
