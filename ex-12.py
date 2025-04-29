e1161204ea8b0b4fc1aa3b74278d97d290118c8b
thiagogcunha86 <thiagogcunha@gmail.com>
2025-04-29 20:51:23 -0300

    Create ex-12.py

---
+++ ex-12.py
@@ -0,0 +1,11 @@
+#12 - Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo.
+
+#Dimensões do retângulo
+base = float(input("Digite o valor da base do retângulo: "))
+altura = float(input("Digite o valor da altura do retângulo: "))
+
+# Calculo Area Total
+area = base * altura
+
+# Resultado
+print(f"A área do retângulo é: {area}")
