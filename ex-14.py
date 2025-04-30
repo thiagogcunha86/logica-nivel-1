e0e0822eded5de0a3e37b4ab13ccd44187e72bdf
thiagogcunha86 <thiagogcunha@gmail.com>
2025-04-29 21:20:52 -0300

    Create ex-14.py

---
+++ ex-14.py
@@ -0,0 +1,9 @@
+#14) Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+# Ler um valor do usuário
+valor = float(input("Digite um valor: "))
+
+# Verificar se o valor é maior que 10
+if valor > 10:
+    print("É MAIOR QUE 10!")
+else:
+    print("NÃO É MAIOR QUE 10!")
